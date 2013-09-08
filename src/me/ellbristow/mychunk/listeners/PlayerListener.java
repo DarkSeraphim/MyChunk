22819387c6d02513215d50898e855a5bce2a5970
ellbristow <admin@ellitopia.net>
2013-09-08 15:28:03 +0200

    Added: Support for [Lease] signs Added: Animal protection (ie Horses) Added: Towny Support Tweak: PVP and PVE tweaks for cross border battles Tweak: Language strings updated

--- src/me/ellbristow/mychunk/listeners/PlayerListener.java
+++ src/me/ellbristow/mychunk/listeners/PlayerListener.java
@@ -1,10 +1,16 @@
 package me.ellbristow.mychunk.listeners;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import me.ellbristow.mychunk.*;
 import me.ellbristow.mychunk.lang.Lang;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -12,9 +18,14 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.*;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.material.Attachable;
+import org.bukkit.material.MaterialData;
 
 public class PlayerListener implements Listener {
+    
+    private HashMap<String, Block> leaseConfirm = new HashMap<String, Block>();
     
     public PlayerListener() {
     }
@@ -31,28 +42,29 @@ public class PlayerListener implements Listener {
             
             Player hurtPlayer = (Player)entity;
             MyChunkChunk chunk = new MyChunkChunk(hurtPlayer.getLocation().getChunk());
+            MyChunkChunk fromChunk = new MyChunkChunk(event.getDamager().getLocation().getChunk());
             
-            if (chunk.isClaimed()) {
+            if (chunk.isClaimed() || fromChunk.isClaimed()) {
                 
                 Entity damager = event.getDamager();
                 
-                if (damager instanceof Player && !chunk.getAllowPVP()) {
+                if (damager instanceof Player && (!chunk.getAllowPVP() || !fromChunk.getAllowPVP())) {
                     
                     event.setCancelled(true);
                     Player naughty = (Player)damager;
                     naughty.sendMessage(ChatColor.RED + Lang.get("NoPermsPVP"));
                     
-                } else if ((damager instanceof Monster || damager instanceof Slime) && !chunk.getAllowMobs()) {
+                } else if ((damager instanceof Monster || damager instanceof Slime) && (!chunk.getAllowMobs() || !fromChunk.getAllowMobs())) {
                     
                     event.setCancelled(true);
                     
                 } else if (damager instanceof Projectile) {
                     
-                    Entity shooter = ((Projectile) event.getDamager()).getShooter();
+                    Entity shooter = ((Projectile)event.getDamager()).getShooter();
                     
-                    if (shooter instanceof Player && entity instanceof Player && !chunk.getAllowPVP()) {
+                    if (shooter instanceof Player && (!chunk.getAllowPVP() || !fromChunk.getAllowPVP())) {
                         event.setCancelled(true);
-                    } else if (shooter instanceof Monster && entity instanceof Player && !chunk.getAllowMobs()) {
+                    } else if (shooter instanceof Monster && !chunk.getAllowMobs()) {
                         event.setCancelled(true);
                     }
                     
@@ -72,7 +84,7 @@ public class PlayerListener implements Listener {
                     
                     Entity shooter = ((Projectile) event.getDamager()).getShooter();
                     
-                    if (shooter instanceof Player && entity instanceof Player && !chunk.getAllowPVP()) {
+                    if (shooter instanceof Player && !chunk.getAllowPVP()) {
                         event.setCancelled(true);
                     }
                     
@@ -80,6 +92,42 @@ public class PlayerListener implements Listener {
                 
             }
             
+        } else if (entity instanceof Animals) {
+            MyChunkChunk chunk = new MyChunkChunk(entity.getLocation().getChunk());
+            
+            if (chunk.isClaimed()) {
+                
+                Entity damager = event.getDamager();
+                
+                if (damager instanceof Player && !chunk.getOwner().equalsIgnoreCase(((Player)damager).getName()) && !chunk.isAllowed(((Player)damager).getName(), "A")) {
+                    
+                    event.setCancelled(true);
+                    Player naughty = (Player)damager;
+                    naughty.sendMessage(ChatColor.RED + Lang.get("NoPermsAnimals"));
+                    
+                } else if (damager instanceof Projectile) {
+                    
+                    Entity shooter = ((Projectile) event.getDamager()).getShooter();
+                    
+                    if (shooter instanceof Player && !chunk.getOwner().equalsIgnoreCase(((Player)shooter).getName()) && !chunk.isAllowed(((Player)shooter).getName(), "A")) {
+                        event.setCancelled(true);
+                    }
+                    
+                }
+                
+            }
+        } else if (entity instanceof Monster || entity instanceof Slime) {
+             MyChunkChunk chunk = new MyChunkChunk(entity.getLocation().getChunk());
+             MyChunkChunk fromChunk = new MyChunkChunk(event.getDamager().getLocation().getChunk());
+            
+            if (!chunk.getAllowMobs() || !fromChunk.getAllowMobs()) {
+                if (event.getDamager() instanceof Player) {
+                    Player damager = (Player)event.getDamager();
+                    if (!damager.hasPermission("mychunk.override")) {
+                        event.setCancelled(true);
+                    }
+                }
+            }
         }
         
     }
@@ -114,7 +162,7 @@ public class PlayerListener implements Listener {
             lang = "NoPermsWater";
         }
         
-        if (!MyChunkChunk.isAllowed(chunk, player, flag) && !WorldGuardHook.isRegion(block.getLocation()) && !FactionsHook.isClaimed(block.getLocation())) {
+        if (!MyChunkChunk.isAllowed(chunk, player, flag) && !WorldGuardHook.isRegion(block.getLocation()) && !FactionsHook.isClaimed(block.getLocation()) && !TownyHook.isClaimed(block.getLocation())) {
             
             player.sendMessage(ChatColor.RED + Lang.get(lang));
             event.setCancelled(true);
@@ -153,7 +201,9 @@ public class PlayerListener implements Listener {
                 
                 // Door
                 
-                if (!MyChunkChunk.isAllowed(chunk, player, "O")) {
+                if (!MyChunkChunk.isAllowed(chunk, player, "O") && getLeaseOwner(event.getClickedBlock()).equals("")) {
+                    
+                    // ANY door
                     
                     player.sendMessage(ChatColor.RED + Lang.get("NoPermsDoor"));
                     OfflinePlayer owner = Bukkit.getServer().getOfflinePlayer(MyChunkChunk.getOwner(chunk));
@@ -164,6 +214,21 @@ public class PlayerListener implements Listener {
                     
                     event.setCancelled(true);
                 
+                } else {
+                    
+                    // Leased Door
+                    
+                    String owner = getLeaseOwner(event.getClickedBlock());
+                    
+                    if (!owner.equals("") && !owner.equalsIgnoreCase(player.getName())) {
+                        
+                        if (!player.hasPermission("mychunk.bypass")) {
+                            player.sendMessage(ChatColor.RED + Lang.get("NoPermsDoor"));
+                            event.setCancelled(true);
+                        }
+                        
+                    }
+                    
                 }
                 
             } else if (block.getTypeId() == 77 || block.getTypeId() == 143) {
@@ -217,7 +282,7 @@ public class PlayerListener implements Listener {
                     
                 }
                 
-            } else if (block.getTypeId() == 61 || block.getTypeId() == 62 || block.getTypeId() == 23 || block.getTypeId() == 117 || block.getTypeId() == 116 || block.getTypeId() == 118 || block.getTypeId() == 120 || block.getTypeId() == 137 || block.getTypeId() == 138 || block.getTypeId() == 140 || block.getTypeId() == 145 || block.getTypeId() == 154 || block.getTypeId() == 158) {
+            } else if (block.getTypeId() == 61 || block.getTypeId() == 62 || block.getTypeId() == 23 || block.getTypeId() == 117 || block.getTypeId() == 116 || block.getTypeId() == 118 || block.getTypeId() == 120 || block.getTypeId() == 137 || block.getTypeId() == 138 || block.getTypeId() == 140 || block.getTypeId() == 145 || block.getTypeId() == 154 || block.getTypeId() == 158 || block.getTypeId() == 92) {
                 
                 // Special Block
                 
@@ -245,6 +310,70 @@ public class PlayerListener implements Listener {
                     
                 }
                 
+            } else if (block.getType().equals(Material.WALL_SIGN)) {
+                
+                // Lease Signs
+                
+                Sign sign = (Sign)block.getState();
+                
+                if (sign.getLine(0).equalsIgnoreCase("[Lease]")) {
+                    
+                    if (!getLeaseOwner(getAttachedBlock(block).getRelative(BlockFace.DOWN)).equals("")) {
+                        
+                        player.sendMessage(ChatColor.GOLD + "Door is leased by " + ChatColor.WHITE + getLeaseOwner(block));
+                        event.setCancelled(true);
+                        
+                    } else {
+                        
+                        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+                            
+                            if(!MyChunk.getToggle("foundEconomy")) {
+                                player.sendMessage(ChatColor.RED + "[Lease] signs require an economy plugin!");
+                                event.setCancelled(true);
+                                return;
+                            }
+                        
+                            double price = Double.parseDouble(sign.getLine(1));
+                            
+                            if (leaseConfirm.containsKey(player.getName()) && leaseConfirm.get(player.getName()).equals(block)) {
+
+                                // Confirm Purchase
+                                
+                                leaseConfirm.remove(player.getName());
+                                
+                                if (!(MyChunkVaultLink.getEconomy().getBalance(player.getName()) >= price)) {
+                                    player.sendMessage(ChatColor.RED + "You cannot afford this Lease!");
+                                    event.setCancelled(true);
+                                    return;
+                                }
+                                
+                                MyChunkVaultLink.getEconomy().withdrawPlayer(player.getName(), price);
+                                
+                                sign.setLine(2, ChatColor.GREEN + player.getName());
+                                Date date = new Date();
+                                Calendar c = Calendar.getInstance();
+                                c.setTime(date);
+                                c.add(Calendar.DATE, 7);
+                                String newDate = new SimpleDateFormat("dd-MM-yyyy").format(c.getTime());
+                                sign.setLine(3, newDate);
+                                sign.update();
+                                
+                                player.sendMessage(ChatColor.GOLD + "You have leased this door for 7 days for "+ChatColor.WHITE + MyChunkVaultLink.getEconomy().format(price) + ChatColor.GOLD+"!");
+
+                            } else {
+
+                                // Unconfirmed, Add
+
+                                player.sendMessage(ChatColor.GOLD + "Right-Click again to confirm Lease Purchase for " + ChatColor.WHITE + MyChunkVaultLink.getEconomy().format(price));
+                                leaseConfirm.put(player.getName(), block);
+
+                            }
+                        }
+                        
+                    }
+                    
+                }
+                
             }
             
         } else if (event.getAction().equals(Action.PHYSICAL)) {
@@ -267,7 +396,7 @@ public class PlayerListener implements Listener {
         }
     }
     
-        @EventHandler (priority = EventPriority.HIGH)
+    @EventHandler (priority = EventPriority.HIGH)
     public void onPlayerJoin(PlayerJoinEvent event) {
             
         if (MyChunk.getToggle("useClaimExpiry")) {
@@ -284,6 +413,10 @@ public class PlayerListener implements Listener {
         
         Location fromLoc = event.getFrom();
         Location toLoc = event.getTo();
+        
+        if (TownyHook.foundTowny() && TownyHook.isClaimed(toLoc)) {
+            return;
+        }
         
         if (fromLoc.getChunk() != toLoc.getChunk()) {
             
@@ -363,19 +496,19 @@ public class PlayerListener implements Listener {
                 
             }
             
-            if (fromChunk.getAllowPVP() != toChunk.getAllowPVP()) {
+            if (!MyChunk.getToggle("preventPVP") && fromChunk.getAllowPVP() != toChunk.getAllowPVP()) {
                 if (toChunk.getAllowPVP()) {
-                    message += ChatColor.DARK_RED + " PVP";
+                    message += ChatColor.DARK_RED + " " + Lang.get("PVP");
                 } else {
-                    message += ChatColor.GREEN + " NoPVP";
+                    message += ChatColor.GREEN + " " + Lang.get("NoPVP");
                 }
             }
             
             if (fromChunk.getAllowMobs() != toChunk.getAllowMobs()) {
                 if (toChunk.getAllowMobs()) {
-                    message += ChatColor.DARK_RED + " Mobs";
+                    message += ChatColor.DARK_RED + " "+Lang.get("Mobs");
                 } else {
-                    message += ChatColor.GREEN + " NoMobs";
+                    message += ChatColor.GREEN + " "+Lang.get("NoMobs");
                 }
             }
             
@@ -460,5 +593,96 @@ public class PlayerListener implements Listener {
         }
         
     }
+    
+    @EventHandler (priority = EventPriority.NORMAL)
+    public void onPlayerAnimalInteract(PlayerInteractEntityEvent event) {
+        
+        if (event.isCancelled()) return;
+        if (!MyChunk.isWorldEnabled(event.getPlayer().getWorld().getName())) return;
+        
+        Entity target = event.getRightClicked();
+        
+        if (target instanceof Animals) {
+            Player player = event.getPlayer();
+            Chunk chunk = target.getLocation().getChunk();
+            String chunkOwner = MyChunkChunk.getOwner(chunk);
+            if (!player.getName().equalsIgnoreCase(chunkOwner) && !MyChunkChunk.isAllowed(chunk, player, "A")) {
+                player.sendMessage(ChatColor.RED + Lang.get("NoPermsAnimals"));
+            }
+            
+        }
+        
+    }
+    
+    @EventHandler (priority = EventPriority.NORMAL)
+    public void onPlayerMount(VehicleEnterEvent event) {
+        
+        if (event.isCancelled()) return;
+        if (!MyChunk.isWorldEnabled(event.getVehicle().getWorld().getName())) return;
+        
+        Entity target = event.getVehicle();
+
+        if (target instanceof Animals) {
+            Player player = (Player)event.getEntered();
+            Chunk chunk = target.getLocation().getChunk();
+            String chunkOwner = MyChunkChunk.getOwner(chunk);
+            if (!player.getName().equalsIgnoreCase(chunkOwner) && !MyChunkChunk.isAllowed(chunk, player, "A")) {
+                player.sendMessage(ChatColor.RED + Lang.get("NoPermsAnimals"));
+                event.setCancelled(true);
+            }
+
+        }
+        
+    }
+    
+    private static String getLeaseOwner(Block door) {
+        
+        if (door.getTypeId() != 64 && door.getTypeId() != 96 && door.getTypeId() != 107) {
+            return "";
+        }
+        
+        Block aboveDoor = door.getRelative(BlockFace.UP);
+                    
+        while (aboveDoor.getTypeId() == 64 || aboveDoor.getTypeId() == 96 || aboveDoor.getTypeId() == 107) {
+            aboveDoor = aboveDoor.getRelative(BlockFace.UP);
+        }
+        
+        BlockFace[] faces = new BlockFace[]{BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST};
+        
+        for (BlockFace face: faces) {
+            
+            Block block = aboveDoor.getRelative(face);
+            BlockState state = block.getState();
+
+            if (state instanceof Sign) {
+                Sign sign = (Sign)state;
+                if (sign.getLine(0).equalsIgnoreCase("[Lease]")) {
+                    String[] line2 = sign.getLine(3).split("-");
+                    int day = Integer.parseInt(line2[0]);
+                    int month = Integer.parseInt(line2[1]);
+                    int year = Integer.parseInt(line2[2]);
+                    if (year >= Calendar.getInstance().get(Calendar.YEAR) || month >= Calendar.getInstance().get(Calendar.MONTH) || day >= Calendar.getInstance().get(Calendar.DAY_OF_MONTH)) {
+                        return ChatColor.stripColor(sign.getLine(2));
+                    } else {
+                        sign.setLine(2, "");
+                        sign.update();
+                    }
+                }
+            }
+            
+        }
+        
+        return "";
+    }
+    
+    private static Block getAttachedBlock(Block b) {
+        MaterialData m = b.getState().getData();
+        BlockFace face = BlockFace.DOWN;
+        if (m instanceof Attachable) {
+            face = ((Attachable) m).getAttachedFace();
+        }
+        return b.getRelative(face);
+    }
+    
     
 }
