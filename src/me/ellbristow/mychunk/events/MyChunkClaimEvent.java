21b80b3943a9b09f8f94edcb5d145028d3fb6c51
ellbristow <admin@ellitopia.net>
2014-07-18 12:14:31 +0200

    Added: Gang chunk claiming support (not fully implemented)

--- src/me/ellbristow/mychunk/events/MyChunkClaimEvent.java
+++ src/me/ellbristow/mychunk/events/MyChunkClaimEvent.java
@@ -10,9 +10,13 @@ public class MyChunkClaimEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
     private LiteChunk chunk;
     private String oldOwner;
+    private boolean isGang = false;
     
-    public MyChunkClaimEvent(String worldName, int x, int z, String oldOwner, String newOwner, boolean chunkForSale) {
-        chunk = new LiteChunk(worldName, x, z, newOwner, chunkForSale);
+    public MyChunkClaimEvent(String worldName, int x, int z, String oldOwner, String newOwner, boolean chunkForSale, String gang) {
+        if (!gang.equals("")) {
+            isGang = true;
+        }
+        chunk = new LiteChunk(worldName, x, z, newOwner, chunkForSale, isGang);
         this.oldOwner = oldOwner;
     }
     
