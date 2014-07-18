2537c4a54e620357f2ecf56a3320d0ce7eb9ebc8
ellbristow <admin@ellitopia.net>
2014-07-18 11:35:15 +0200

    Tweak: Made the Vault link a static class/method

--- src/me/ellbristow/mychunk/utils/MyChunkVaultLink.java
+++ src/me/ellbristow/mychunk/utils/MyChunkVaultLink.java
@@ -1,33 +1,24 @@
 package me.ellbristow.mychunk.utils;
 
-import me.ellbristow.mychunk.MyChunk;
 import net.milkbowl.vault.economy.Economy;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class MyChunkVaultLink {
     
-    private static MyChunk plugin;
-    public static Economy economy = null;
-    public boolean foundEconomy = false;
-    public String economyName = "";
+    public static Economy economy;
+    public static boolean foundEconomy;
+    public static String economyName;
     
-    public MyChunkVaultLink (MyChunk instance) {
-        plugin = instance;
-        initEconomy();
-    }
-    
-    private void initEconomy() {
-        RegisteredServiceProvider<Economy> economyProvider = plugin.getServer().getServicesManager().getRegistration(Economy.class);
+    public static void initEconomy() {
+        RegisteredServiceProvider<Economy> economyProvider = Bukkit.getServer().getServicesManager().getRegistration(Economy.class);
         if (economyProvider != null) {
             economy = economyProvider.getProvider();
         }
         if (economy != null) {
             foundEconomy = true;
-            economyName = plugin.getServer().getServicesManager().getRegistration(net.milkbowl.vault.economy.Economy.class).getProvider().getName();
+            economyName = economy.getName();
         }
     }
-    
-    public static Economy getEconomy() {
-        return economy;
-    }
+
 }
